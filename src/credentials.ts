9a6e00cfc2a276dab718aaad25e8dd34a271faf4
paulettemal <136779147+paulettemal@users.noreply.github.com>
2025-01-11 17:20:59 -0500

    Add files via upload

---
+++ src/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAD8deKtFl_lczv4cDQ6sge3TZGzDlUvlQ",
+    authDomain: "hibrida-8f9d8.firebaseapp.com",
+    projectId: "hibrida-8f9d8",
+    storageBucket: "hibrida-8f9d8.firebasestorage.app",
+    messagingSenderId: "49721249707",
+    appId: "1:49721249707:web:88ac2247475623597b8cee",
+    measurementId: "G-WMF49QZ3TZ"
+};
